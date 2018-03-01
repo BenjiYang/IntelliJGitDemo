61723d540af3e127d85e8b57a54de23366b481be
BengiOnMAC <356844587@qq.com>
2018-03-01 22:59:08 +0800

    Update from Local branch

--- src/com/benji/Main.java
+++ src/com/benji/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-	System.out.println("I'm writing msg from REMOTE MASTER");
+	    System.out.println("I'm writing msg from REMOTE MASTER");
+	    System.out.println("From LOCAL my_cool_feature");
     }
 }
