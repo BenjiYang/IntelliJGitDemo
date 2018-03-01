248564c048f705177fac3bd9ec88a8958959a504
BengiOnMAC <356844587@qq.com>
2018-03-01 23:19:45 +0800

    test2 commit my_cool_feature

--- src/com/benji/Main.java
+++ src/com/benji/Main.java
@@ -3,12 +3,14 @@ package com.benji;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-	    System.out.println("I'm writing msg from REMOTE MASTER");
-	    System.out.println("From LOCAL my_cool_feature");
+        // write your code here
+        System.out.println("I'm writing msg from REMOTE MASTER");
+        System.out.println("From LOCAL my_cool_feature");
 
-	    //test1 commit my_cool_feature
+        //test1 commit my_cool_feature
 
         //test2 commit my_cool_feature
+
+        //test3 commit my_feature...
     }
 }
